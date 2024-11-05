5642b7dae68ab3a08835ca88884c9affee36a797
matheusghb <mistermquack124@gmail.com>
2024-11-05 17:40:38 -0300

    oi

---
+++ navigation/navigation.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { createNativeNavigator } from '@react-navigation/native-stack'
+
+const Stack = createNativeNavigator();
+
+export default function navigation() {
+    return (
+        <Stack.Navigator>'</Stack.Navigator>
+    )
+}
